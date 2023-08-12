7f4c357901a6ab7014c24e09a35a357e77d7c7cf
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-08-12 22:50:38 +0100

    Handling Errors

--- script.js
+++ script.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+  </article>
+
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1
+};
 //////////////////////
 
 // Older Method of calling APIs
@@ -40,35 +64,104 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('germany');
 // getCountryData('japan');
 
-const getCountryData = function(country) {
-const request = new XMLHttpRequest();
-request.open('GET', `https://restcountries.com/v2/name/${country}`);
-request.send();
+////////////////////////
+// CallBack Hell (It means having a callback function inside another CB function, inside another CB function);
+// const getCountryDataAndNeighbour = function (country) {
+//   //AJAX Call country 1
+//   const request = new XMLHttpRequest(); // Older method of calling APIs
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-  const html = `
-    <article class="country">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-    </article>
+//     //Render country 1
+//     renderCountry(data);
 
-    `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+//     // Get neighbour country 2
+//     const [neighbour] = data.borders;
+
+//     if (!neighbour) return;
+
+//     //AJAX Call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryDataAndNeighbour('nigeria');
+
+///////////////////////////////
+// Promises and Fetch API
+
+// Newer Method of calling API
+const request = fetch(`https://restcountries.com/v2/name/nigeria`);
+console.log(request);
+
+///////////// Expanded Format (Promise and Handler Format) /////////
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       //using the 'then' method after fetching an API means handling the promise.
+//       console.log(response);
+//       return response.json();//'.json means the response will be transformed to json, which produces another promise of its own...
+//       ... hence the next 'then' handler method.
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+////////// Simple Promise and Handler Format ////////////////////////
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0]));
+// };
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => {
+      console.log(response);
+
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      response.json();
+    })
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json()) //This is called when the promise is fufilled
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      //This is called when the promise is rejected
+      console.error(`${err}💣💣💣`);
+      renderError(`Something went wrong 💣💣💣 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      // This is always called whether the promise is fufilled or rejected
+      //It is used for an event that always needs to happen, no matter the promise result e.g loading spinner
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('nigeria');
 });
 
-}
-
-getCountryData('nigeria');
-getCountryData('canada');
-getCountryData('germany');
-getCountryData('japan');
+getCountryData('bfbafbgb');
