f741b1aa82ed5f8ba8c0f611a60b05550ee7f6db
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-08-13 20:50:00 +0100

    Code challenge in progress

--- other.js
+++ other.js
@@ -1,4 +1,19 @@
 'use strict';
 
+///////////////////////////////
 
-////
+// Challenge #1
+
+const whereAmI = function (lat, lng) {
+ const request = fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+ .catch(err => {
+    console.log(`File not found (${err.status})`);
+ }).finally(() => console.log(`There could be an error`))
+ 
+ console.log(request);
+    if(!request) throw new Error(`Location not found`);
+    console.log(`You're in Berlin, Germany`);
+    
+};
+
+whereAmI(52.508, 13.381);
