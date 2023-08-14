752ddf5a87880cf7a5b084648f393c2aae289782
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-08-14 19:01:08 +0100

    Challenge Done

--- other.js
+++ other.js
@@ -17,12 +17,11 @@
 //        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
 //        </div>
 //    </article>
- 
+
 //    `;
 //    countriesContainer.insertAdjacentHTML('beforeend', html);
 //    // countriesContainer.style.opacity = 1;
 //  };
-
 
 // const whereAmI = function(lat, lng) {
 //    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
@@ -36,7 +35,7 @@
 
 //       return fetch(`https://restcountries.com/v2/name/${data.country}`)
 //    })
-//    then(response => {  
+//    then(response => {
 //             if (!response.ok)
 //               throw new Error(`Country not found (${response.status})`);
 //             return response.json();
@@ -46,3 +45,62 @@
 // }
 
 // whereAmI(52.508, 13.381);
+
+///////////////////////
+// Code Challenge #2
+
+const imgContainer = document.querySelector('.images');
+let currentImg;
+
+const createImage = function (imgPath) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img'); //How to create image from JS
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found!'));
+    });
+  });
+};
+
+const wait = function(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds * 1000)
+    });
+};
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg')
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display ='none';
+    return createImage('img/img-3.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 3 loaded');
+    return wait(2)
+  })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+  .catch(err => console.error(err));
