9b28075da9f2bbe546fc4d17f0ec49ecbe35067c
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-08-14 22:55:00 +0100

    Returning values

--- other.js
+++ other.js
@@ -49,58 +49,58 @@
 //////////////////////////
 // Code Challenge #2
 
-const imgContainer = document.querySelector('.images');
-let currentImg;
+// const imgContainer = document.querySelector('.images');
+// let currentImg;
 
-const createImage = function (imgPath) {
-  return new Promise((resolve, reject) => {
-    const img = document.createElement('img'); //How to create image from JS
-    img.src = imgPath;
+// const createImage = function (imgPath) {
+//   return new Promise((resolve, reject) => {
+//     const img = document.createElement('img'); //How to create image from JS
+//     img.src = imgPath;
 
-    img.addEventListener('load', function () {
-      imgContainer.append(img);
-      resolve(img);
-    });
+//     img.addEventListener('load', function () {
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
 
-    img.addEventListener('error', function () {
-      reject(new Error('Image not found!'));
-    });
-  });
-};
+//     img.addEventListener('error', function () {
+//       reject(new Error('Image not found!'));
+//     });
+//   });
+// };
 
-const wait = function(seconds) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, seconds * 1000)
-    });
-};
+// const wait = function(seconds) {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, seconds * 1000)
+//     });
+// };
 
-createImage('img/img-1.jpg')
-  .then(img => {
-    currentImg = img;
-    console.log('Image 1 loaded');
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded');
 
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none';
-    return createImage('img/img-2.jpg')
-  })
-  .then(img => {
-    currentImg = img;
-    console.log('Image 2 loaded');
-
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display ='none';
-    return createImage('img/img-3.jpg');
-  })
-  .then(img => {
-    currentImg = img;
-    console.log('Image 3 loaded');
-    return wait(2)
-  })
+//     return wait(2);
+//   })
 //   .then(() => {
 //     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg')
 //   })
-  .catch(err => console.error(err));
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded');
+
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display ='none';
+//     return createImage('img/img-3.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 3 loaded');
+//     return wait(2)
+//   })
+// //   .then(() => {
+// //     currentImg.style.display = 'none';
+// //   })
+//   .catch(err => console.error(err));
