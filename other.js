4902da9cc1efb3a49163441f1c7f48d90f9e4b93
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-08-15 11:53:05 +0100

    Final Challenge done

--- other.js
+++ other.js
@@ -49,30 +49,30 @@
 //////////////////////////
 // Code Challenge #2
 
-// const imgContainer = document.querySelector('.images');
+const imgContainer = document.querySelector('.images');
 // let currentImg;
 
-// const createImage = function (imgPath) {
-//   return new Promise((resolve, reject) => {
-//     const img = document.createElement('img'); //How to create image from JS
-//     img.src = imgPath;
+const createImage = function (imgPath) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img'); //How to create image from JS
+    img.src = imgPath;
 
-//     img.addEventListener('load', function () {
-//       imgContainer.append(img);
-//       resolve(img);
-//     });
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
 
-//     img.addEventListener('error', function () {
-//       reject(new Error('Image not found!'));
-//     });
-//   });
-// };
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found!'));
+    });
+  });
+};
 
-// const wait = function(seconds) {
-//     return new Promise((resolve) => {
-//         setTimeout(resolve, seconds * 1000)
-//     });
-// };
+const wait = function (seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
 // createImage('img/img-1.jpg')
 //   .then(img => {
@@ -104,3 +104,44 @@
 // //     currentImg.style.display = 'none';
 // //   })
 //   .catch(err => console.error(err));
+
+///////////////////////////////////
+///////// Code Challenge #3 /////////////
+
+const loadNPause = async function () {
+  try {
+    // Part 1
+    // Load Image 1
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    // Load Image 2
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// loadNPause();
+
+//////// Part 2 ///////////
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = await imgArr.map(async img => await createImage(img)); // It executes the loop function one after the other 
+
+    // promise.all is  used to get the returning value
+    const imgEL = await Promise.all(imgs);
+    console.log(imgEL);
+
+    imgEL.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
